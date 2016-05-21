80a76ee57b03eecedd814afa753534f77413b604
HARADA REO <rharada@golyat.local>
2016-05-22 03:34:38 +0900

    first commit

---
+++ TODOLIST/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  TODOLIST
+//
+//  Created by HARADA REO on 2015/09/28.
+//  Copyright © 2015年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
